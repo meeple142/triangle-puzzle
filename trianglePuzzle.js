6ab5615124602147af3699a3f5a957ec576edae5
Joshua McKinney <jmckinney0040@gmail.com>
2017-07-11 21:40:54 -0600

    have a board maker

---
+++ trianglePuzzle.js
@@ -0,0 +1,14 @@
+var boardTree;
+
+function makeBoard(boardArray) {
+    var boardOut = {
+        children: []
+    };
+
+    if (boardIn) {
+        boardOut.board: boardArray
+    } else {
+        boardOut: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    }
+    return boardOut;
+}
