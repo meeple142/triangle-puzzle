0fc3e0fcc5c15f1d5710416e45511a20e4fb9f08
Joshua McKinney <meeple142@users.noreply.github.com>
2018-01-30 01:25:43 -0700

    no longer a tree fixes #1. got an answer!

--- trianglePuzzle.js
+++ trianglePuzzle.js
@@ -2,35 +2,29 @@
 /*eslint no-undef:2, no-unused-vars:0, no-console:0*/
 //process.stdin.resume(); //so the program will not close instantly
 
-function exitHandler(options, err) {
-    if (options.cleanup) console.log('clean');
-    if (err) console.log(err.stack);
-
-    process.exit();
-}
-
-//do something when app is closing
-process.on('exit', exitHandler.bind(null, {
-    cleanup: true
-}));
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {
-    exit: true
-}));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {
-    exit: true
-}));
 
 function toSum(sum, item) {
     return sum + item;
 }
 
-var fs = require('fs'),
+function makeBoardList(){
+   var listOut = [],
+   rowCount = 15;
+   
+   for(i = 0; i < rowCount; ++i){
+      listOut.push([]);
+   }
+   
+   return listOut;
+}
+
+var row, index,
+    fs = require('fs'),
     util = require('util'),
-    boardTree,
+    boardList = makeBoardList(),
+    links = [],
+    boardCount = 0,
+    boardStateCount = 1,
     moves = [
         //right to bottom left
         [0, 2, 5],
@@ -82,43 +76,44 @@ var fs = require('fs'),
         [5, 4, 3]
     ];
 
+
+
 function makeBoard(boardIn) {
-    var boardOut = {},
-        pegCount;
+    var boardOut = {};
+        
+        //incrament the number of boards and make id
+							boardCount += 1;
+        boardOut.id = boardCount; 
 
     if (boardIn) {
         //copy
-        boardOut.board = boardIn.board.map(function (ele) {
+        boardOut.pegs = boardIn.pegs.map(function (ele) {
             return ele;
         });
-        boardOut.numberOfPegs = -1;
     } else {
         //empty constructor
-        boardOut.board = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-        boardOut.numberOfPegs = 14;
+        boardOut.pegs = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     }
 
-    boardOut.solved = false
-    boardOut.children = [];
     return boardOut;
 }
-var counter = {
-    loop: 0,
-    boards: 1
-};
 
-function makeMoves(boardIn) {
-    counter.loop += 1;
-    console.log(counter);
+
+function makeLink(parentId, childId){
+   links.push({
+      parent: parentId,
+      child: childId
+   })
+}
+
+function getChildBoards(boardIn) {
     var childrenOut;
 
+			//will return a copy ofthe board with the move done to it or null
     function makeMove(boardInL, jumperPegIndex, removedPegIndex, landingSpaceIndex) {
-
-
-
-        var jumperPeg = boardInL.board[jumperPegIndex],
-            removedPeg = boardInL.board[removedPegIndex],
-            landingSpace = boardInL.board[landingSpaceIndex],
+        var jumperPeg = boardInL.pegs[jumperPegIndex],
+            removedPeg = boardInL.pegs[removedPegIndex],
+            landingSpace = boardInL.pegs[landingSpaceIndex],
 
             shouldMakeMove = removedPeg === 1 && jumperPeg === 1 && landingSpace === 0,
             newBoard, pegCount;
@@ -127,14 +122,9 @@ function makeMoves(boardIn) {
             //copy the board
             newBoard = makeBoard(boardInL);
             //then make the move
-            newBoard.board[jumperPegIndex] = 0;
-            newBoard.board[removedPegIndex] = 0;
-            newBoard.board[landingSpaceIndex] = 1;
-
-            //check are we done?
-            pegCount = newBoard.board.reduce(toSum, 0);
-            newBoard.numberOfPegs = pegCount;
-            newBoard.solved = pegCount === 1;
+            newBoard.pegs[jumperPegIndex] = 0;
+            newBoard.pegs[removedPegIndex] = 0;
+            newBoard.pegs[landingSpaceIndex] = 1;
 
             return newBoard;
         }
@@ -144,34 +134,70 @@ function makeMoves(boardIn) {
 
 
 
-    childrenOut = moves.map(function (move) {
+     return moves.reduce(function (children, move) {
             //will make a copy ofthe board with the move done to it or null
-            //console.log(move)
-            return makeMove(boardIn, move[0], move[1], move[2]);
-        })
-        .filter(function (board) {
-            //remove out all the nulls - not valid moves
-            return board !== null;
-        });
+            var child = makeMove(boardIn, move[0], move[1], move[2]);
+            
+            if(child !== null){
+               children.push(child);
+            }
+            
+            return children; 
+            
+        }, []);
 
-    //console.log('c', childrenOut)
-    boardIn.children = childrenOut
-    //    boardIn.children = boardIn.children.concat(childrenOut)
-    if (boardIn.children.length > 0 && counter.loop < 1000) {
-        counter.boards += boardIn.children.length;
-        boardIn.children.forEach(function (child) {
-            makeMoves(child);
-        });
-    }
-    //console.log(counter);
 }
 
-boardTree = makeBoard();
-makeMoves(boardTree)
-fs.writeFileSync('boardTree.json', util.inspect(boardTree, {
-    depth: null
-}), 'utf8');
+// make sure every peg matches
+function boardsMatch(a,b){
 
-//console.dir(boardTree, {
-//    depth: null
-//});
+   return a.pegs.every((peg, i)=> peg === b.pegs[i]);
+
+}
+
+
+
+// Add first board to board list
+boardList[14].push(makeBoard());
+var childBoards, childId;
+
+// loop the rows in boardList, the row value means the number of pegs thoes boards have
+// this is to speed up checking if a board state already exsitis
+for(row = 14; row > 0; --row){
+    console.log('number of boards:', boardCount);
+    console.log('number of board states:', boardStateCount);
+    console.log();
+    for(index = 0; index < boardList[row].length ; ++index){
+        
+        //get the Childboards
+        childBoards = getChildBoards(boardList[row][index]);
+        childBoards.forEach((childBoard)=>{
+        
+        	    //find the match if it has one
+            var dupBoard = boardList[row - 1].find(boardListBoard=>{
+            			return boardsMatch(childBoard, boardListBoard)
+            })
+        	
+        				// no dup
+        				if(typeof dupBoard === "undefined"){
+        					   childId = childBoard.id;
+        					   //add to boardList
+        					   boardList[row - 1].push(childBoard) 
+        					   boardStateCount += 1;
+        				} else {
+        					   childId = dupBoard.id;
+        				}
+        				
+           //make link with correct ids
+           	makeLink(boardList[row][index].id, childId);	
+        })
+    }
+}
+
+
+console.log("Number of solution states:", boardList[1].length)
+console.log("Number of links:", links.length)
+
+fs.writeFileSync('states.json', JSON.stringify(boardList, null, 4), 'utf8');
+fs.writeFileSync('links.json', JSON.stringify(links, null, 4), 'utf8');
+
